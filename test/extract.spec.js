74a9462e59c718200f593bb9731b68623d9a41be
Patrick Gaubatz <patrick@gaubatz.at>
2015-05-24 19:46:59 +0200

    more tests

--- test/extract.spec.js
+++ test/extract.spec.js
@@ -9,13 +9,17 @@ var StringStream = require('./utils/string-stream');
 var extract = require('../lib/extract');
 
 describe('extract()', function () {
-  it('should return passthrough plaintext', function (done) {
+  it('should passthrough plaintext', function (done) {
     var hello = 'hello world';
     extract('text/plain', new StringStream([hello]))
       .on('data', function (chunk) {
         chunk.toString().should.equal(hello);
         done();
       });
+  });
+  it('should throw an error for a non-extractable MIME type', function () {
+    var mime = 'no/such/mime/type';
+    extract.bind(null, mime).should.throw(Error, 'No matching extractor for MIME type ' + mime);
   });
   it('should extract hosts files from archives', function (done) {
     var zipFile = path.join(__dirname, 'fixtures', 'hosts-2.zip');
@@ -26,6 +30,15 @@ describe('extract()', function () {
       })
       .once('end', function () {
         content.should.have.string('localhost');
+        done();
+      });
+  });
+  it('should emit an error and close the read stream for unparsable archives', function (done) {
+    extract('application/zip', new StringStream(['this is no zip']))
+      .on('error', function (err) {
+        err.should.be.an.instanceof(Error);
+      })
+      .on('end', function () {
         done();
       });
   });
