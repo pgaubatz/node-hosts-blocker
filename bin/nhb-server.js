c68c58cccff08699eb355ec4728203e3ba034ac1
Patrick Gaubatz <patrick@gaubatz.at>
2015-05-28 13:31:29 +0200

    make port of HTTP server overridable

--- bin/nhb-server.js
+++ bin/nhb-server.js
@@ -9,6 +9,7 @@ var hostsBlocker = require('../index');
 program
   .version(require('../package.json').version)
   .option('-d, --listen-ip <ip>', 'Listening IP address (required)')
+  .option('-p, --listen-port <port>', 'Listening port', 80)
   .option('-u, --ui-hostname <hostname>', 'Hostname for the Web-UI', 'hosts-blocker')
   .parse(process.argv);
 
@@ -16,9 +17,14 @@ if (!program.listenIp) {
   program.help();
 }
 
-hostsBlocker.createServer(program.listenIp, program.uiHostname)
+hostsBlocker.createServer(program.listenIp, program.listenPort, program.uiHostname)
   .once('error', function (err) {
     console.error('Cannot start HTTP server:', err);
   });
 
-console.log('Host-Blocker\'s Web-UI is available at: http://' + program.uiHostname);
+var hostPort = program.uiHostname;
+if (program.listenPort !== 80) {
+  hostPort += ':' + program.listenPort;
+}
+
+console.log('Host-Blocker\'s Web-UI is available at: http://' + hostPort);
